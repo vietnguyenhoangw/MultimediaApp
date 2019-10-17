85a20bf431901f7d05c24479f3e7c4923c9f77b6
vietnguyenhoangw <vietnguyenhoangw@gmail.com>
2019-10-17 10:27:23 +0700

    [UPD] - :heavy_check_mark:,:hammer: add permision check, and refactor code

--- app/src/main/java/com/example/changeprofilepicapp/MainActivity.java
+++ app/src/main/java/com/example/changeprofilepicapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.changeprofilepicapp;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.FileProvider;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -28,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
     private int PICK_PHOTO_REQUEST_CODE = 200;
     public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1001;
+    private static final int REQUEST = 1000;
     private File photoFile;
 
     @Override
@@ -45,8 +50,8 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /* Create bottom sheet (UI, listener...) */
     public void bottomSheet() {
-
         String[] item = new String[] {
                 "Camera",
                 "Photos",
@@ -77,83 +82,146 @@ public class MainActivity extends AppCompatActivity {
                 .show();
     }
 
-    private void onLaunchCamera() {
-// tạo Intent để truy cập Camera
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-// tạo file lưu hình ảnh và truy xuất trong tương lai
-        String photoFileName = "IMG_" + System.currentTimeMillis();
-        photoFile = getPhotoFileUri(photoFileName);
-// bao bọc file bởi ContentProvider
-        Uri fileProvider = FileProvider.getUriForFile(this, "com.example.changeprofilepicapp", photoFile);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-// thực hiện Intent để chụp ảnh
-            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-        }
+    private boolean checkPermissions() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+            return true;
+        return false;
     }
 
-    public File getPhotoFileUri(String fileName) {
-// Dùng “getExternalFilesDir” để không cần request external read/write runtime permissions
-        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "MuiltimediaApp");
-// Tạo thư mục chứa hình ảnh nếu chưa tồn tại
-        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
-            Log.d("MuiltimediaApp", "failed to create directory");
-        }
-// Trả về đối tượng file tương ứng với file name
-        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
-        return file;
+
+    /* request permissions */
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this,
+                new String[] {
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                },
+                REQUEST);
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST && grantResults.length == 2 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            onLaunchCamera();
+            onPickPhoto();
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
+    /* launch camera */
+    private void onLaunchCamera() {
+        if (checkPermissions()) {
+            requestPermissions();
+        }
+        else {
+            /* Create Intent to access the camera */
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            /* ceate save images file and access in future */
+            String photoFileName = "IMG_" + System.currentTimeMillis();
+            photoFile = getPhotoFileUri(photoFileName);
+
+            // bao bọc file bởi ContentProvider
+            /* cover file by content provider */
+            Uri fileProvider = FileProvider.getUriForFile(this, "com.example.changeprofilepicapp", photoFile);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+
+                // start intent to take photo
+                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+            }
+        }
+    }
+
+    /* getPhotoFile*/
+    public File getPhotoFileUri(String fileName) {
+        /* using "getExternalFileDir" to not need to use request external read/write runtime permissions */
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "MuiltimediaApp");
+
+        /* Create save images file if file is not exist */
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d("MuiltimediaApp", "failed to create directory");
+        }
+
+        /* return the file object corresponding to file name */
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+
+    /* pic image */
+    private void onPickPhoto() {
+        if (checkPermissions()) {
+            requestPermissions();
+        }
+        else {
+            /* Create intent to pick images from Gallery */
+            Intent intent = new Intent(Intent.ACTION_PICK,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+            if (intent.resolveActivity(getPackageManager()) != null) {
+            /* start activity to get images */
+                startActivityForResult(intent, PICK_PHOTO_REQUEST_CODE);
+            }
+        }
+    }
+
+    /*
+    *  take launch camera action or picimages action
+    * */
+    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        /*
+         Requestcode equal take photo
+         */
         if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
-// tại thời điểm này, đã có hình ảnh từ camera lưu trên file
+                /* at this time, images is avaiable from camera to save on file*/
                 Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
 
-                /* chup anh vao bo nho */
+                /* take photo to Storage */
                 MediaStore.Images.Media.insertImage(
                         getContentResolver(),
                         takenImage,
                         "demo_image",
                         "demo_image"
                 );
-// RESIZE BITMAP, để tránh lỗi OutOfMemoryError khi hiển thị ảnh lên UI
+                /* RESIZE BITMAP, to  avoidOutOfMemoryError when show image on UI */
                 Bitmap resizedBitmap = BitmapScaler.scaleToFitWidth(takenImage, 200);
-// Hiển thị hình ảnh vừa chụp lên image view
+
+                /* setimage was taken on imageview */
                 profileImage.setImageBitmap(takenImage);
-            } else { // Xử lý cho trường hợp không chụp ảnh (chọn Cancel ở Camera app)
+
+            } else { /* handle in picture was not taken case. */
                 Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
             }
         }
 
+        /*
+         Requestcode equal pick a photo
+         */
         if (requestCode == PICK_PHOTO_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 if (data != null) {
                     Uri photoUri = data.getData();
-// Lấy uri của hình ảnh từ data
+                    /* get image's uri from data */
                     Bitmap selectedImage = null;
-                    try { // Tạo đối tượng bitmap tương ứng với uri
+                    try { // create bitmap image corresponding to uri
                         selectedImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-// Hiển thị hình ảnh (bitmap) được chọn lên image view
+                    /* set image (bitmap) on imageview */
                     if (selectedImage != null) {
                         profileImage.setImageBitmap(selectedImage);
                     }
                 }
             }
-        }
-    }
-
-    private void onPickPhoto() {
-// Tạo Intent để chọn hình ảnh từ Gallery
-        Intent intent = new Intent(Intent.ACTION_PICK,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-// thực hiện Intent để chọn ảnh
-            startActivityForResult(intent, PICK_PHOTO_REQUEST_CODE);
         }
     }
 }
